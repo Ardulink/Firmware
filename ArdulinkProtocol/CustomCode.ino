f3cd284532398334192c9e62d4b1937a51301e73
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-04-19 11:41:09 +0200

    handleKprs function signature fix

--- ArdulinkProtocol/CustomCode.ino
+++ ArdulinkProtocol/CustomCode.ino
@@ -4,21 +4,10 @@ bool handleCustomMessage(String customId, String value) {
   return commandHandledSuccessfully;
 }
 
-bool handleKprs(const String& params, size_t length) {
+bool handleKprs(const char* cParams, size_t length) {
   // here you can write your own code. For instance the commented code change pin intensity if you press 'a' or 's'
   // take the command and change intensity on pin 11 this is needed just as example for this sketch
   
-//  static int intensity = 0;
-//  char commandChar = params.charAt(3);
-//  if (commandChar == 'a') { // If press 'a' less intensity
-//    intensity = max(0, intensity - 1);
-//    analogWrite(11, intensity );
-//    return true;
-//   } else if (commandChar == 's') { // If press 's' more intensity
-//    intensity = min(125, intensity + 1);
-//    analogWrite(11, intensity );
-//    return true;
-//  }
   bool commandHandledSuccessfully = false;
   return commandHandledSuccessfully;
 }
--- examples/custom/CustomMessagesChipKit/CustomMessagesChipKit.ino
+++ examples/custom/CustomMessagesChipKit/CustomMessagesChipKit.ino
@@ -60,7 +60,7 @@ bool handleCustomMessage(String customId, String value) {
   return true;
 }
 
-bool handleKprs(const String& params, size_t length) {
+bool handleKprs(const char* cParams, size_t length) {
   // here you can write your own code. For instance the commented code change pin intensity if you press 'a' or 's'
   // take the command and change intensity on pin 11 this is needed just as example for this sketch
   
