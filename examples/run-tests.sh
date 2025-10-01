e1e6ba2d975521690ebca34aadeba11e41693809
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-10-01 20:29:18 +0200

    EchoCustomMessages test added

--- examples/run-tests.sh
+++ examples/run-tests.sh
@@ -2,6 +2,9 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+export SKETCH_FILE=$SCRIPT_DIR/custom/EchoCustomMessages/
+behave $SKETCH_FILE/tests/
+
 export SKETCH_FILE=$SCRIPT_DIR/custom/JoystickCustomMessages/
 behave $SKETCH_FILE/tests/
 
